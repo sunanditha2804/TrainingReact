99918971a45b8efef80929c1a2dea543c64642ac
sunanditha2804 <2211cs010575@mallareddyuniversity.ac.in>
2025-02-03 12:53:29 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let username="Ratna";
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 style={{ color: 'red' }}>Hello World</h1>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h1 style={{ color: 'red' }}>Hello World</h1>
+    //   </header>
+    // </div>
+    <div>
+      <h1>{username}</h1>
+      <p>{100+100}</p>  
     </div>
   );
 }
